cb87ab6f9a362292db7175deeb9ec7632a150ef2
Abhilash Anil <2020.abhilash@gmail.com>
2017-08-27 10:52:06 -0400

    Data from first run of wiki data finder

--- wiki_information_finder.py
+++ wiki_information_finder.py
@@ -10,77 +10,81 @@ import json
 import codecs
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os.path
+import hashlib
 
-def get_caption(table):
-    return table.caption.text;
-
-def get_personal_info_json(table):
-    personal_info_json = {}
-    personal_info_str = ''
+def cache_page(url, page):
+    hash_object = hashlib.md5(url.encode())
+    filename = 'cache_' + hash_object.hexdigest() + '.json'
     
-    rows = table.find_all('tr')
+    with codecs.open(filename, 'w', 'utf-8-sig') as f:
+        f.write(str(page.content))
+    f.closed
     
-    for row in rows:
-        if len(row.find_all('th')) != 0:
-            attrs = row.th.attrs
-            
-            flag = False
-            for attr in attrs:
-                if attr == 'scope':
-                    flag = True
-                    break
-            
-            if flag == True:
-                if row.th['scope'] == 'row':
-                    key = row.th.text.replace('\n', ' ').replace('  ', ' ').replace('|', ' ').replace('=', '')
-                    value = row.td.text.replace('\n', ' ').replace('  ', ' ').replace('|', ' ').replace('=', '')
-                    personal_info_str = personal_info_str + key
-    return personal_info_json
-
+def get_page(url):
+    status = False
+    hash_object = hashlib.md5(url.encode())
+    filename = 'cache_' + hash_object.hexdigest() + '.json'
+    
+    if os.path.exists(filename):
+        with codecs.open(filename, 'r', 'utf-8-sig') as f:
+            page = f.read()
+        f.closed
+        status = True
+    else:
+        page = requests.get(url)
+        if page.status_code == 200:
+            cache_page(url, page)
+            page = page.content
+            status = True
+        
+    return status, page
+    
 def get_info(url):
     info = ''
-    wiki_page = requests.get(url)
+    status, wiki_page = get_page(url)
     
-    if wiki_page.status_code == 200:
-        wiki_soup = BeautifulSoup(wiki_page.content, 'html.parser')
+    if status:
+        wiki_soup = BeautifulSoup(wiki_page, 'html.parser')
         info_tables = wiki_soup.find_all('table', { 'class': 'infobox vcard' })
         if len(info_tables) == 0:
             info = 'infobox vcard not found'
         else:
             for info_table in info_tables:
-                info = info + str(info_table)
-                '''
-                with codecs.open('data.html', 'a', 'utf-8-sig') as f:
-                    f.write(str(info_table))
-                f.closed
-                '''
+                info = info + '\n' + str(info_table)
     else:
         info = 'error connecting'
     
     return info
-
+    
 def find_related_links(link):
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    related_links = soup.find_all('a')
     urls = []
     
-    for related_link in related_links:
-        flag = False
-        for attr in related_link.attrs:
-            if attr == 'href':
-                flag = True
-                break
-        if flag == True:
-            if related_link['href'].startswith('/wiki/'):
-                urls.append(related_link['href'])
+    status, page = get_page(link)
+    
+    if status:
+        soup = BeautifulSoup(page, 'html.parser')
+        related_links = soup.find_all('a')
+        
+        for related_link in related_links:
+            flag = False
+            for attr in related_link.attrs:
+                if attr == 'href':
+                    flag = True
+                    break
+            if flag == True:
+                if related_link['href'].startswith('/wiki/'):
+                    urls.append(related_link['href'])
                 
     return urls
 
 # Starting Point
+start = datetime.now()
+print('Started app: ' + str(start))
+
 wiki_url = 'https://en.wikipedia.org'
 url_part = '/wiki/Main_Page'
-depth = 1000
+depth = 10000
 
 full_url = wiki_url + url_part
 
@@ -89,16 +93,27 @@ soup = BeautifulSoup(page.content, 'html.parser')
 links = soup.find_all('a')
 urls = []
 data = {}
+url_index = 0
+urls.append(full_url)
 
 while len(urls) < depth:
-    related_links = find_related_links(full_url)
+    print('Current depth: ' + str(len(urls)))
+    
+    related_links = find_related_links(urls[url_index])
+    url_index = url_index + 1
+    
+    print('Related urls found: ' + str(len(related_links)))
     for related_link in related_links:
         if related_link not in urls:
-            urls.append(related_link)
+            urls.append(wiki_url + related_link)
 
-with codecs.open('data.json', 'a', 'utf-8-sig') as f:
+with codecs.open('data.json', 'w', 'utf-8-sig') as f:
     for url in urls:
         data['url'] = url
-        data['info'] = get_info(wiki_url + url)
+        data['info'] = get_info(url)
         json.dump(data, f)
-f.closed        
+f.closed
+
+end = datetime.now()
+print('Ended app: ' + str(end))
+
